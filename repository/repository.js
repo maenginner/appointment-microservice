a22fefd2898cdac5e5b2c98c78f901f99c13fb18
Mauricio Giraldo <mauricio.giraldoacosta@gmail.com>
2018-05-17 15:53:01 -0500

    Changing function

--- repository/repository.js
+++ repository/repository.js
@@ -1,11 +1,10 @@
 import models from '../models/index';
 
-class Repository {
-  getAppointment(id) {
-    return models.Appointment.findOne({ where: id });
-  }
+
+function getAppointment(id) {
+  return models.Appointment.findOne({ where: id });
 }
 
 module.exports = {
-  Repository: new Repository(),
+  getAppointment,
 };
