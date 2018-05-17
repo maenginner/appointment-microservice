1ea838a2ca0250ff4f2f3ad36fe0909dd013c4dd
Mauricio Giraldo <mauricio.giraldoacosta@gmail.com>
2018-05-17 11:23:52 -0500

    using Repository for testing

--- resolvers/appointment.js
+++ resolvers/appointment.js
@@ -1,10 +1,14 @@
-export default{
-    Query:{
-        getAppointment:(parent,{id},{models}) => models.Appointment.findOne({where:{id}}),
-        allAppointments:(parent,args,{models}) => models.Appointment.findAll(),
-    },
+import Repository from '../repository/repository';
 
-    Mutation:{
-        createAppointment:(parent,args,{models}) => models.Appointment.create(args)
-    }
-}
+export default{
+  Query: {
+    getAppointment: (parent, { id }) => {
+      Repository.getAppointment({ id });
+    },
+    allAppointments: (parent, args, { models }) => models.Appointment.findAll(),
+  },
+
+  Mutation: {
+    createAppointment: (parent, args, { models }) => models.Appointment.create(args),
+  },
+};
